d31db61657c067e9b0615c547ae11c56d738afca
adiybukhori <muhammadadiy93@gmail.com>
2026-05-04 14:03:16 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+{
+  "name": "sppp-ro-portal",
+  "private": true,
+  "version": "0.0.0",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "react": "latest",
+    "react-dom": "latest",
+    "tailwindcss": "latest",
+    "@tailwindcss/vite": "latest"
+  },
+  "devDependencies": {
+    "vite": "latest",
+    "@vitejs/plugin-react": "latest"
+  }
+}
